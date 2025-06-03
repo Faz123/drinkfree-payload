f4738f68063d811dc606b8f784229181fbdb7520
Chris Farrelly <Chrisjfarrelly@gmail.com>
2025-06-03 21:17:01 +0100

    create migrations

---
+++ src/migrations/index.ts
@@ -0,0 +1,9 @@
+import * as migration_20250603_201631 from './20250603_201631';
+
+export const migrations = [
+  {
+    up: migration_20250603_201631.up,
+    down: migration_20250603_201631.down,
+    name: '20250603_201631'
+  },
+];
