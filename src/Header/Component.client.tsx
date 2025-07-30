27f388a12933db88755ecd394e79f5d2cc6bd808
Chris Farrelly <Chrisjfarrelly@gmail.com>
2025-07-30 23:09:13 +0100

    mega menu

--- src/Header/Component.client.tsx
+++ src/Header/Component.client.tsx
@@ -4,6 +4,7 @@ import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 import React, { useEffect, useState } from 'react'
 import Logo from '@/../public/logo-white.svg'
+import { Menu, X } from 'lucide-react'
 
 import type { Header } from '@/payload-types'
 
@@ -19,6 +20,8 @@ export const HeaderClient: React.FC<HeaderClientProps> = ({ data }) => {
   const { headerTheme, setHeaderTheme } = useHeaderTheme()
   const pathname = usePathname()
 
+  const [menuOpen, setMenuOpen] = useState(false)
+
   useEffect(() => {
     setHeaderTheme(null)
     // eslint-disable-next-line react-hooks/exhaustive-deps
@@ -29,14 +32,36 @@ export const HeaderClient: React.FC<HeaderClientProps> = ({ data }) => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [headerTheme])
 
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen)
+  }
+
+  // close menu on page load
+
+  useEffect(() => {
+    setMenuOpen(false)
+  }, [pathname])
+
   return (
-    <header className="container relative z-20   " {...(theme ? { 'data-theme': theme } : {})}>
-      <div className="py-8 flex justify-between">
-        <Link href="/" className="mr-8">
-          <Logo className="w-full max-w-52 h-auto" />
-        </Link>
-        <HeaderNav data={data} />
-      </div>
-    </header>
+    <>
+      <header className="container relative z-20" {...(theme ? { 'data-theme': theme } : {})}>
+        <div className="py-8 flex justify-between">
+          <Link href="/" className="mr-8">
+            <Logo className="w-full max-w-52 h-auto" />
+          </Link>
+          <button onClick={toggleMenu}>
+            <Menu size={32} />
+          </button>
+        </div>
+      </header>
+      {menuOpen && (
+        <div className="absolute bg-background h-full w-full z-40">
+          <button onClick={toggleMenu} className="absolute top-10 right-4">
+            <X size={32} />
+          </button>
+          <HeaderNav data={data} />
+        </div>
+      )}
+    </>
   )
 }
--- src/Header/Nav/index.tsx
+++ src/Header/Nav/index.tsx
@@ -12,9 +12,9 @@ export const HeaderNav: React.FC<{ data: HeaderType }> = ({ data }) => {
   const navItems = data?.navItems || []
 
   return (
-    <nav className="flex gap-3 items-center">
+    <nav className="flex flex-col gap-3 items-start my-16 max-w-[80%] m-auto">
       {navItems.map(({ link }, i) => {
-        return <CMSLink key={i} {...link} appearance="link" className="text-2xl" />
+        return <CMSLink key={i} {...link} appearance="link" className="text-4xl md:text-8xl" />
       })}
       {/* <Link href="/search">
         <span className="sr-only">Search</span>
