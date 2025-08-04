4ab5a42dd85789c153f744c0894728f7c2eb498f
Chris Farrelly <Chrisjfarrelly@gmail.com>
2025-08-04 21:16:45 +0100

    add menu animation

--- src/Header/Nav/index.tsx
+++ src/Header/Nav/index.tsx
@@ -7,12 +7,22 @@ import type { Header as HeaderType } from '@/payload-types'
 import { CMSLink } from '@/components/Link'
 import Link from 'next/link'
 import { SearchIcon } from 'lucide-react'
+import gsap from 'gsap'
+import { useGSAP } from '@gsap/react'
+import { useRef } from 'react'
 
 export const HeaderNav: React.FC<{ data: HeaderType }> = ({ data }) => {
   const navItems = data?.navItems || []
+  const container = useRef<HTMLDivElement>(null)
+  useGSAP(
+    () => {
+      gsap.fromTo(container.current, { y: 100, opacity: 0 }, { y: 0, opacity: 1 })
+    },
+    { scope: container },
+  )
 
   return (
-    <nav className="flex flex-col gap-3 items-start my-16 max-w-[80%] m-auto">
+    <nav ref={container} className="flex flex-col gap-3 items-start my-16 max-w-[80%] m-auto">
       {navItems.map(({ link }, i) => {
         return <CMSLink key={i} {...link} appearance="link" className="text-4xl md:text-8xl" />
       })}
