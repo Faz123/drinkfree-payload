3cd06c62f04b60918c28a62ea59135b99055f6e0
Chris Farrelly <Chrisjfarrelly@gmail.com>
2025-08-30 17:49:48 +0100

    remove before login component

--- src/app/(payload)/admin/importMap.js
+++ src/app/(payload)/admin/importMap.js
@@ -27,7 +27,6 @@ import { RowLabel as RowLabel_1f6ff6ff633e3695d348f4f3c58f1466 } from '@/Footer/
 import { default as default_2f6c954033a571f1fc2b0f44dd03b717 } from '@/components/DrinkFreeIcon'
 import { default as default_692cc047147eb26803058dcf9fb54cad } from '@/components/DrinkFreeLogo'
 import { default as default_1a7510af427896d367a49dbf838d2de6 } from '@/components/BeforeDashboard'
-import { default as default_8a7ab0eb7ab5c511aba12e68480bfe5e } from '@/components/BeforeLogin'
 import { S3ClientUploadHandler as S3ClientUploadHandler_f97aa6c64367fa259c5bc0567239ef24 } from '@payloadcms/storage-s3/client'
 
 export const importMap = {
@@ -60,6 +59,5 @@ export const importMap = {
   "@/components/DrinkFreeIcon#default": default_2f6c954033a571f1fc2b0f44dd03b717,
   "@/components/DrinkFreeLogo#default": default_692cc047147eb26803058dcf9fb54cad,
   "@/components/BeforeDashboard#default": default_1a7510af427896d367a49dbf838d2de6,
-  "@/components/BeforeLogin#default": default_8a7ab0eb7ab5c511aba12e68480bfe5e,
   "@payloadcms/storage-s3/client#S3ClientUploadHandler": S3ClientUploadHandler_f97aa6c64367fa259c5bc0567239ef24
 }
